5c3571d56b23276bfd47234b57e34a58e58ec319
Cesar Quispe <vqc1909a@gmail.com>
2020-03-19 18:21:56 -0500

    Autocomplete VueJS

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about flex flex-col items-center">
+    <div class="absolute inset-0 z-0" @click="modal = false"></div>
+    <input type="text" class="bg-gray-300 px-4 py-2 z-10" autocomplete="off" v-model="state"   @focus="modal = true">
+    <div v-if="filteredStates && modal" class="z-10">
+      <ul class="w-48 bg-gray-800 text-white">
+        <li  class="py-2 border-b cursor-pointer" v-for="filteredState in filteredStates" :key="filteredState" @click="setState(filteredState)">{{filteredState}}</li>
+      </ul>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+      state: '',
+      modal: false,
+      states: [
+        'Florida', 'Alabama', 'Alaska', 'Texas'
+      ],
+      filteredStates: [
+      ],
+    }
+  },
+  mounted(){
+    this.filterStates();
+  },
+  watch: {
+    state(){
+      this.filterStates();
+    }
+  },
+  methods: {
+    filterStates(){
+        if(this.state.length == 0){
+          this.filteredStates = this.states;
+        }
+        this.filteredStates = this.states.filter(state => {
+          return state.toLowerCase().startsWith(this.state.toLowerCase());
+        })
+    },
+    setState(state){
+      this.state = state;
+      this.modal = false;
+    }
+  }
+}
+</script>
