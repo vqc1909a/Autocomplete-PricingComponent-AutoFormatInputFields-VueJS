ffef1566fda4359a4b491e89432860a1ee19db61
Cesar Quispe <vqc1909a@gmail.com>
2020-03-21 01:35:42 -0500

    Control

--- src/components/Telephone.vue
+++ src/components/Telephone.vue
@@ -1,42 +1,48 @@
 <template>
-     <div class="mt-6">
-          <input type="text" :placeholder="template" class="w-56 text-2xl bg-gray-300 p-3 rounded-lg focus:ouline-none" v-model="number">     
-     </div>
+  <div class="mt-6">
+    <input
+      type="text"
+      :placeholder="template"
+      class="w-56 text-2xl bg-gray-300 p-3 rounded-lg focus:ouline-none"
+      v-model="number"
+    />
+  </div>
 </template>
 
 <script>
-     export default {
-          name: "Telephone",
-          props: {
-               template: String,
-          },
-          data(){
-               return {
-                    number: '',
-                    format: '', 
-                    regex: '',
+export default {
+  name: "Telephone",
+  props: {
+    template: String
+  },
+  data() {
+    return {
+      number: "",
+      format: "",
+      regex: ""
+    };
+  },
+  mounted() {
+    let x = 1;
+    this.format = this.template.replace(/X+/g, () => "$" + x++);
 
-               }
-          },
-          mounted(){
-               let x = 1;
-               this.format = this.template.replace(/X+/g, ()=> '$' + x++);
-
-               this.template.match(/X+/g).forEach((char) => {
-                    this.regex+= '(\\d{'+char.length+'})?';
-               })
-               console.log(this.regex);
-          },
-          watch: {
-               number(){
-                    this.number = this.number.replace(/[^0-9]/g, '')
-                                             .replace(new RegExp(this.regex), this.format)
-                                             .substr(0, this.template.length);
-               }
-          }
-     }
+    this.template.match(/X+/g).forEach(char => {
+      this.regex += "(\\d{" + char.length + "})?";
+    });
+    console.log(this.regex);
+  },
+  watch: {
+    number(next, prev) {
+      if (next.length > prev.length) {
+        this.number = this.number
+          .replace(/[^0-9]/g, "")
+          .replace(new RegExp(this.regex), this.format)
+          .substr(0, this.template.length);
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
